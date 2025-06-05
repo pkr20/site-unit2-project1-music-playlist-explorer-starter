19282d31319c2cba2883b87f3d8be78c768a5ecc
Kayleen Ramirez <kayleenr@meta.com>
2025-06-05 09:51:15 -0700

    README.md added; functional modal for all playlist articles

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,19 @@
+const modal = document.getElementById("playlist-modal")
+const closeButton = document.getElementsByClassName("close")[0];
 
+function openModal(playlist){
+    document.getElementById('playlist-name').innerText = playlist.title;
+    document.getElementById('playlist-img').src = playlist.imageUrl;
+    document.getElementById('creator-name').innerText = `Creator: ${playlist.creator}`;
+    modal.style.display = 'block'
+}
+
+closeButton.onclick = function() {
+    modal.style.display = "none"
+}
+
+window.onclick = function(event) {
+    if (event.target == modal){
+        modal.style.display = "none"
+    }
+}
